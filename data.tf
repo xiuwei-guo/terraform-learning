b02b723c90d123db07a681863567f2617f92a01a
xiuwei.guo <xiuwei.guo@foxmail.com>
2023-05-02 11:48:50 +0800

    add data for oci_identity_availability_domains

--- data.tf
+++ data.tf
@@ -6,3 +6,7 @@ data "oci_core_images" "list_image" {
   operating_system_version = 7
   state = "AVAILABLE"
 }
+
+data "oci_identity_availability_domains" "availability_domains" {
+  compartment_id = var.compartment_id
+}
--- output.tf
+++ output.tf
@@ -1,10 +1,14 @@
-# output "print_image_id" {
-#   value = data.oci_core_images.list_image.images[*].id 
-#   description = "the last version image id"
-# }
+output "print_image_id" {
+  value = data.oci_core_images.list_image.images[*].id 
+  description = "the last version image id"
+}
+
+output "vcn_id" {
+  value = oci_core_vcn.internal.id
+  description = "the vcn id"
+}
 
 
-# output "vcn_id" {
-#   value = oci_core_vcn.internal.id
-#   description = "the vcn id"
-# }
+output "availability_domain_list" {
+  value = data.oci_identity_availability_domains.availability_domains[*].id
+}
